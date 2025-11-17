cd015aa73842c51261f8140f52a9a3ce3f4256e3
Koushik <koushikruidas@gmail.com>
2025-11-17 17:21:40 +0530

    added auto configuration, no more componentScan annotation needed.

---
+++ src/main/java/com/poinciana/loganalyzerClient/config/LogAnalyzerAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.poinciana.loganalyzerClient.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.poinciana.loganalyzerClient")
+public class LogAnalyzerAutoConfiguration {
+}
